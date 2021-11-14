8e582e7fd8b704e1cc4a40a810c002852ba74d9a
hotmiamy <mrllps07@gmail.com>
2021-11-14 17:09:00 -0300

    remove mac return 0

--- ft_types.c
+++ ft_types.c
@@ -6,7 +6,7 @@
 /*   By: llopes-n <llopes-n@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 00:41:55 by llopes-n          #+#    #+#             */
-/*   Updated: 2021/11/14 16:58:43 by llopes-n         ###   ########.fr       */
+/*   Updated: 2021/11/14 17:08:33 by llopes-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int	ft_type_p(unsigned long ptr)
 	char	*str;
 	char	*temp;
 
-	if (ptr == 0 && IS_MACOS == 0)
+	if (ptr == 0)
 		return (write(1, "(nil)", 5));
 	str = ft_base_converter(ptr, "0123456789abcdef");
 	temp = str;
